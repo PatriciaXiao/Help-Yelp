95d542d63f9eec25e4edbadd864d50e883d3239c
Patricia Xiao <patricia.xiao@berkeley.edu>
2018-11-18 23:17:29 -0800

    init my repository

---
+++ Patricia/readme.md
@@ -0,0 +1,5 @@
+# List of Done and Todo
+
+## To do
+
+* Try collaborative filtering
---
+++ Patricia/standardCF.py
@@ -0,0 +1,6 @@
+############################################
+# Standard Collaborative Filtering
+#      Patricia at 11.18, 2018
+############################################
+
+import pandas as pd
